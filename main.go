487f8cfec0d340fb77fdc6cb95b906f0803e657d
dimamaisoftwire <dima.mai@softwire.com>
2025-07-08 13:43:05 +0100

    implemented classic fizzbuzz

--- main.go
+++ main.go
@@ -4,7 +4,19 @@ import "fmt"
 
 // This is our main function, this executes by default when we run the main package.
 func main() {
+
 	fmt.Println("Hello, World!")
+	for i := 0; i < 100; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 
 	// Put your code here...
 }
